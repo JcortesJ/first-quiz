825757ec1442631b3a46b3aca87147233b365c7c
juan <jcortesj@unal.edu.co>
2023-10-30 23:16:35 -0500

    q2

--- part1/question2.py
+++ part1/question2.py
@@ -15,7 +15,14 @@
 # it. As always, there is a test suite that checks the result. It is in 
 # `question2_test.py.`
 
-swapper = None
+def swap(tup):
+  if len(tup) == 2:
+    #we only redefine the tuple with the desired order
+    tup = tup[1],tup[0]
+    return tup
+  else:
+    return 0
+swapper = swap
 
 def run_swapper(list_of_tuples):
   return list(map(swapper, list_of_tuples))
